cacb5fe70f3d73ce07082e4c06edd80a3ae2635a
Larry Anderson <larryboymi@hotmail.com>
2016-10-14 11:07:56 -0500

    parse seconds to delay

--- app.js
+++ app.js
@@ -11,9 +11,18 @@ const sendResponse = require('./src/slack/sendResponse')
 
 const mission = (params) =>
   getUserInfo(config['bot-token'])(params.user_id)
-  .then((user) => sendMessage(config['bot-token'])(params.channel_id, params.user_name, user.profile.image_48, params.text))
-  .then(() => findMessage(config['bot-token'])(params.channel_id, params.user_name, params.text))
-  .then((msg) => deleteMessage(config['bot-token'])(params.channel_id, msg.ts, 5000))
+  .then((user) => {
+    const textParts = params.text.split(' ')
+    const defaultDelay = isNaN(textParts[0])
+    const waitSeconds = defaultDelay ? 5 : (Math.abs(textParts[0]) > 60 ? 60 : Math.abs(textParts[0]))
+    if (!defaultDelay) {
+      textParts.shift()
+    }
+    const messageText = textParts.join(' ')
+    return sendMessage(config['bot-token'])(params.channel_id, params.user_name, user.profile.image_48, messageText)
+    .then(() => findMessage(config['bot-token'])(params.channel_id, params.user_name, messageText))
+    .then((msg) => deleteMessage(config['bot-token'])(params.channel_id, msg.ts, waitSeconds * 1000))
+  })
 
 exports.handler = (event, context) =>
   decrypt(new Buffer(config['encrypted-token'], 'base64'))
